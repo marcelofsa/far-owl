2856180a5fff4d91e52f0b101b39217f70ec071c
jkdrangel@gmail.com <jkdrangel@gmail.com@cec8ff61-9af5-ba29-63f6-4982105dfa92>
2013-10-24 21:17:52 +0000

    Testando Subversion

--- trunk/ModelFarowl/src/model/Conta.java
+++ trunk/ModelFarowl/src/model/Conta.java
@@ -12,8 +12,61 @@ public abstract class Conta {
     
     private String nome;
     private String login;
-    private String senha;
+    private int senha;
     private String email;
+
+    public Conta(String nome, String login, int senha, String email) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
     
+    
+    
+    public boolean fazerLogin(String login, int senha){
+        
+        
+        
+        return false;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Conta{" + "nome=" + nome + '}';
+    }
+
     
 }
