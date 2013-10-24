1a03fc3b89f9976b78744fa8291e60313aa98220
jkdrangel@gmail.com <jkdrangel@gmail.com@cec8ff61-9af5-ba29-63f6-4982105dfa92>
2013-10-24 21:48:45 +0000

    Criando Classes

---
+++ trunk/ModelFarowl/src/model/Administrador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author CasaPc
+ */
+public class Administrador extends Conta{
+
+    public Administrador(String nome, String login, int senha, String email) {
+        super(nome, login, senha, email);
+    }
+    
+    
+}
