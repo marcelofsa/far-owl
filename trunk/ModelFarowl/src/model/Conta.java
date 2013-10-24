94e0dd691d5475bea4ea615d4f92417b7d581621
jkdrangel@gmail.com <jkdrangel@gmail.com@cec8ff61-9af5-ba29-63f6-4982105dfa92>
2013-10-24 21:05:57 +0000

    Model iniciado pq paulo não colocou o projeto completo ainda

---
+++ trunk/ModelFarowl/src/model/Conta.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author CasaPc
+ */
+public abstract class Conta {
+    
+    private String nome;
+    private String login;
+    private String senha;
+    private String email;
+    
+    
+}
