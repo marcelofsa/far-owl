08a023b6a085ac782ed0795668304500fa0393fe
jkdrangel@gmail.com <jkdrangel@gmail.com@cec8ff61-9af5-ba29-63f6-4982105dfa92>
2013-10-26 16:57:13 +0000

    Pequenas modificações sem muita importacia (é muito chato inventar uma descrição para cada commit)

---
+++ trunk/ModelFarowl/src/model/AvisoEvent.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author CasaPc
+ */
+public class AvisoEvent extends EventObject{
+
+    public AvisoEvent(Object o) {
+        super(o);
+    }
+    
+}
