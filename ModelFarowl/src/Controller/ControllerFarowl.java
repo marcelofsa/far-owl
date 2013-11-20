17d1e1a0a08eefdd8cde054a40d48a40ed12975b
penaecomp <penaecomp@cec8ff61-9af5-ba29-63f6-4982105dfa92>
2013-11-20 00:54:08 +0000

    Mais do Controller

---
+++ ModelFarowl/src/Controller/ControllerFarowl.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author CasaPc
+ */
+public class ControllerFarowl{
+
+    /**
+     *
+     * @param nome
+     * @param login
+     * @param senha
+     * @param email
+     */
+    public ControllerFarowl() {
+    }
+    
+    public GerarRanking(){
+    	
+    }
+    
+    public Avisar(){
+    	
+    }
+     
+    public String EncriptarSenha(){
+    	return new String ();
+    }
+    
+    public boolean FiltrarPost(){
+    	return true;
+    }
+}
